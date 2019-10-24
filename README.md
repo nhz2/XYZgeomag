3cfc63969cabb43bc63564d95c7e418ba26bd72a
nhz2 <nhz2@cornell.edu>
2019-10-24 00:42:57 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # geomag
 Lightweight C++ header-only library for calculating the magnetic field on earth. Compatible with CUDA and Arduino.
+
+Calculates the magnetic field around earth in the International Terrestrial Reference System(ITRS).
+
+Unlike most World Magnetic Model(WMM) software, which uses latitude, longitude, and altitude inputs to calculate the North East Down(NED) components of the magnetic field, this library uses geocentric cartesian coordinates as input, and outputs the magnetic field in the same geocentric cartesian coordinate system as the inputs.
+
+## Error
+
+geomag is within 10nT of the official WMM software.
+
+## Performance
+
+geomag uses single precision floating points. It's designed to minimize ram usage for embedded systems.
+
+| Device      | Speed    |
+|-------------|----------|
+| Arduino Uno | 50 ms    |
+| Teensy 3.6  |  80 \mus |
+|             |          |
+
+## Using geomag
+
+Just download `geomag.hpp` and include it.
+Here is an example Arduino sketch:
+
+~~~cpp
+#include "geomag.hpp"
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(1,INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int val= digitalRead(1);
+  geomag::Vector in;
+  in.x=val+1128529.6885767058f;
+  in.y=val+0.0;
+  in.z=val+6358023.736329913f;
+  geomag::Vector out;
+  int starttime=micros();
+  int starttimemil=millis();
+  out=geomag::GeoMag(2017.5,in,geomag::WMM2015);
+  int endtime=micros();
+  int endtimemil=millis();
+  Serial.println(out.x*1E9);
+  Serial.println(out.y*1E9);
+  Serial.println(out.z*1E9);
+  Serial.print("time in micro seconds: ");
+  Serial.println(endtime-starttime);
+  Serial.print("time in milli seconds: ");
+  Serial.println(endtimemil-starttimemil);
+  delay(2000);
+}
+~~~
+
+
+
+## Adding New Coefficents
+
+To add new coefficents, download the new `.COF` file from [https://www.ngdc.noaa.gov/geomag/WMM/DoDWMM.shtml](https://www.ngdc.noaa.gov/geomag/WMM/DoDWMM.shtml)
+
+Add the .COF file to the `test_codegen` directory.
+
+Then run for example
+`python wmmcodeupdate.py -f WMM2015.COF -f WMM2015v2.COF -o ../geomag.hpp -n 12` from the `test_codegen` directory.
+
+In this example, `WMM2015.COF` and `WMM2015v2.COF` are the `.COF` files to use in `geomag.hpp`.
---
+++ test_codegen/wmmcodeupdate.py
@@ -0,0 +1,338 @@
+# MIT License
+#
+# Copyright (c) 2019 Nathan Zimmerberg
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+#23 OCT 2019
+#wmmcodeupdate.py
+#This is a simple script to parse WMM.COF into a c++ header file
+#To run, use command "python wmmcodeupdate.py", add -h flag for help
+#it un Schmidt semi-normalizes the coefficents and stores them by:
+#    index=((2*maxdegree-m+1)*m)/2+n
+import math
+
+def main(infilenames, headerfilename, maxdegree):
+    """parse infilenames into headerfilename c++ header file
+
+    The coefficents are un Schmidt semi-normalized.
+    indexing is by ((2*maxdegree-m+1)*m)/2+n
+    Args:
+        infilenames(list of filenames): the .COF files that contains the
+            WMM coefficents, download this from https://www.ngdc.noaa.gov/geomag/WMM/DoDWMM.shtml
+        headerfilename(string ending in .hpp): the c++ header file.
+            Stored in a ConstModel, index=((2*maxdegree-m+1)*m)/2+n
+                typedef struct {
+                        const float epoch;
+                        const float Main_Field_Coeff_C[NUMCOF];
+                        const float Main_Field_Coeff_S[NUMCOF];
+                        const float Secular_Var_Coeff_C[NUMCOF];
+                        const float Secular_Var_Coeff_S[NUMCOF];
+                } ConstModel;
+        maxdegree(positive integer): maximum degree"""
+    outstr= header_file_header(headerfilename,maxdegree)
+    for infilename in infilenames:
+        data= parseescof(infilename,maxdegree)
+        dyear= data[0]
+        cofs= data[1:]
+        c_cofs=[0]*(((maxdegree+1)*(maxdegree+2))//2)
+        s_cofs=[0]*(((maxdegree+1)*(maxdegree+2))//2)
+        c_secvars=[0]*(((maxdegree+1)*(maxdegree+2))//2)
+        s_secvars=[0]*(((maxdegree+1)*(maxdegree+2))//2)
+        for cof in cofs:
+            n= cof[0]
+            m= cof[1]
+            g= cof[2]
+            h= cof[3]
+            gsec= cof[4]
+            hsec= cof[5]
+            if (m==0):
+                unnorm= 1.0
+            else:
+                unnorm= math.sqrt(2.0*float(math.factorial(n-m))/float(math.factorial(n+m)))
+            c_cofs[((2*maxdegree-m+1)*m)//2+n]= g*unnorm
+            s_cofs[((2*maxdegree-m+1)*m)//2+n]= h*unnorm
+            c_secvars[((2*maxdegree-m+1)*m)//2+n]= gsec*unnorm
+            s_secvars[((2*maxdegree-m+1)*m)//2+n]= hsec*unnorm
+        outstr = outstr + header_file_model_code(infilename[:-4],dyear,c_cofs,s_cofs,c_secvars,s_secvars)
+    outstr = outstr + "}\n#endif /* __GEOMAG_HPP */"
+    with open(headerfilename,'w') as f:
+        f.write(outstr)
+
+
+def header_file_header(headerfilename,maxdegree):
+    """returns the header of the header file
+
+    Args:
+        headerfilename(string ending in .hpp): the c++ header file with the coefficents
+            Stored in a ConstModel, index=((2*maxdegree-m+1)*m)/2+n
+                typedef struct {
+                        const float epoch;
+                        const float Main_Field_Coeff_C[NUMCOF];
+                        const float Main_Field_Coeff_S[NUMCOF];
+                        const float Secular_Var_Coeff_C[NUMCOF];
+                        const float Secular_Var_Coeff_S[NUMCOF];
+                } ConstModel;
+        maxdegree(positive integer): maximum degree"""
+    head="""/*
+MIT License
+
+Copyright (c) 2019 Nathan Zimmerberg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+// %s Generated by python script wmmcodeupdate.py
+/** \\file
+ * \\author Nathan Zimmerberg (nhz2@cornell.edu)
+ * \\date 23 OCT 2019
+ * \\brief Header-only library to calculate the magnetic field in the International Terrestrial Reference System(ITRS).
+ * \\details Designed to minimize ram usage for embedded systems.
+
+The data from WMM2015, is not subject to copyright protection.
+Modifications are:
+  using ITRS coordinates,
+  conversion from nT to T,
+  Using unnormalized coefficents genrated by the python script wmmcodeupdate.py
+  using a different spherical harmonics calculation, described in sections 3.2.4 and 3.2.5:
+    Satellite Orbits Models, Methods and Applications,
+      by Oliver Montenbruck and Eberhard Gill 2000
+*/
+#ifndef __GEOMAG_HPP
+#define __GEOMAG_HPP
+
+#include <math.h>
+
+#ifdef __CUDACC__
+#define CUDA_CALLABLE_MEMBER __host__ __device__
+#else
+#define CUDA_CALLABLE_MEMBER
+#endif
+
+namespace geomag
+{
+constexpr int NMAX= %d;//order of the Model
+constexpr int NUMCOF= (NMAX+1)*(NMAX+2)/2;//number of coefficents
+struct ConstModel{
+    float epoch;//decimal year
+    float Main_Field_Coeff_C[NUMCOF];
+    float Main_Field_Coeff_S[NUMCOF];
+    float Secular_Var_Coeff_C[NUMCOF];
+    float Secular_Var_Coeff_S[NUMCOF];
+    /** Function for indexing the C spherical component n,m at dyear time.*/
+    CUDA_CALLABLE_MEMBER inline float C(int n, int m, float dyear) const{
+      int index= (m*(2*NMAX-m+1))/2+n;
+      #ifdef PROGMEM
+        return pgm_read_float_near(Main_Field_Coeff_C+index)+(dyear-epoch)*pgm_read_float_near(Secular_Var_Coeff_C+index);
+      #endif /* PROGMEM */
+      return Main_Field_Coeff_C[index]+(dyear-epoch)*Secular_Var_Coeff_C[index];
+    }
+    /** Function for indexing the S spherical component n,m at dyear time.*/
+    CUDA_CALLABLE_MEMBER inline float S(int n, int m, float dyear) const{
+      int index= (m*(2*NMAX-m+1))/2+n;
+      #ifdef PROGMEM
+        return pgm_read_float_near(Main_Field_Coeff_S+index)+(dyear-epoch)*pgm_read_float_near(Secular_Var_Coeff_S+index);
+      #endif /* PROGMEM */
+      return Main_Field_Coeff_S[index]+(dyear-epoch)*Secular_Var_Coeff_S[index];
+    }
+};
+//mean radius of  ellipsoid in meters from section 1.2 of the WMM2015 Technical report
+constexpr float EARTH_R= 6371200.0f;
+
+typedef struct {
+    float x;
+    float y;
+    float z;
+} Vector;
+
+/** Return the magnetic field in International Terrestrial Reference System coordinates, units Tesla.
+ INPUT:
+    position_itrs(Above the surface of earth): The location where the field is predicted, units m.
+    dyear(should be around the epoch of the model): The decimal year, for example 2015.0
+    WMM(): Magnetic field model to use.
+ */
+CUDA_CALLABLE_MEMBER inline Vector GeoMag(float dyear,Vector position_itrs, const ConstModel& WMM){
+    float x= position_itrs.x;
+    float y= position_itrs.y;
+    float z= position_itrs.z;
+    float px= 0;
+    float py= 0;
+    float pz= 0;
+    float rsqrd= x*x+y*y+z*z;
+    float temp= EARTH_R/rsqrd;
+    float a= x*temp;
+    float b= y*temp;
+    float f= z*temp;
+    float g= EARTH_R*temp;
+
+    int n,m;
+    //first m==0 row, just solve for the Vs
+    float Vtop= EARTH_R/sqrtf(rsqrd);//V0,0
+    float Wtop= 0;//W0,0
+    float Vprev= 0;
+    float Wprev= 0;
+    float Vnm= Vtop;
+    float Wnm= Wtop;
+
+    //iterate through all ms
+    for ( m = 0; m < NMAX+1; m++)
+    {
+        // iterate through all ns
+        for (n = m; n <= NMAX+1; n++)
+        {
+            if (n==m){
+                if(m!=0){
+                    temp= Vtop;
+                    Vtop= (2*m-1)*(a*Vtop-b*Wtop);
+                    Wtop= (2*m-1)*(a*Wtop+b*temp);
+                    Vprev= 0;
+                    Wprev= 0;
+                    Vnm= Vtop;
+                    Wnm= Wtop;
+                }
+            }
+            else{
+                temp= Vnm;
+                float invs_temp=1.0f/((float)(n-m));
+                Vnm= ((2*n-1)*f*Vnm - (n+m-1)*g*Vprev)*invs_temp;
+                Vprev= temp;
+                temp= Wnm;
+                Wnm= ((2*n-1)*f*Wnm - (n+m-1)*g*Wprev)*invs_temp;
+                Wprev= temp;
+            }
+            if (m<NMAX && n>=m+2){
+                px+= 0.5f*(n-m)*(n-m-1)*(WMM.C(n-1,m+1,dyear)*Vnm+WMM.S(n-1,m+1,dyear)*Wnm);
+                py+= 0.5f*(n-m)*(n-m-1)*(-WMM.C(n-1,m+1,dyear)*Wnm+WMM.S(n-1,m+1,dyear)*Vnm);
+            }
+            if (n>=2 && m>=2){
+                px+= 0.5f*(-WMM.C(n-1,m-1,dyear)*Vnm-WMM.S(n-1,m-1,dyear)*Wnm);
+                py+= 0.5f*(-WMM.C(n-1,m-1,dyear)*Wnm+WMM.S(n-1,m-1,dyear)*Vnm);
+            }
+            if (m==1 && n>=2){
+                px+= -WMM.C(n-1,0,dyear)*Vnm;
+                py+= -WMM.C(n-1,0,dyear)*Wnm;
+            }
+            if (n>=2 && n>m){
+                pz+= (n-m)*(-WMM.C(n-1,m,dyear)*Vnm-WMM.S(n-1,m,dyear)*Wnm);
+            }
+        }
+    }
+    return {-px*1.0E-9f,-py*1.0E-9f,-pz*1E-9f};
+}
+// Model parameters\n"""%(headerfilename,maxdegree)
+    return head
+
+def header_file_model_code(modelname, dyear,c_cofs,s_cofs,c_secvars,s_secvars):
+    """return the code defining the WMM coefficents and model
+                Stored in a ConstModel, index=((2*maxdegree-m+1)*m)/2+n
+                    typedef struct {
+                            const float epoch;
+                            const float Main_Field_Coeff_C[NUMCOF];
+                            const float Main_Field_Coeff_S[NUMCOF];
+                            const float Secular_Var_Coeff_C[NUMCOF];
+                            const float Secular_Var_Coeff_S[NUMCOF];
+                    } ConstModel;
+
+    Args:
+        modelname(str, a valid C++ name): name of the model
+        dyear(positive float): the year of the magnetic model, ex 2015.0
+        c_cofs,s_cofs,c_secvars,s_secvars(list of floats): coefficents of the model"""
+
+    head="""constexpr
+#ifdef PROGMEM
+    PROGMEM
+#endif /* PROGMEM */
+ConstModel %s = {%f,\n"""%(modelname,dyear)
+    modeltail= '};\n\n'
+    cs='{'
+    ss='{'
+    csec='{'
+    ssec='{'
+    for i in range(len(c_cofs)):
+        cs+= repr(c_cofs[i])+','
+        ss+= repr(s_cofs[i])+','
+        csec+= repr(c_secvars[i])+','
+        ssec+= repr(s_secvars[i])+','
+    cs= cs[:-1]+'}'
+    ss= ss[:-1]+'}'
+    csec= csec[:-1]+'}'
+    ssec= ssec[:-1]+'}'
+    return head+cs+',\n'+ss+',\n'+csec+',\n'+ssec+modeltail
+
+
+def parseescof(infilename, maxdegree):
+    """return a list of lists from the infilename cof data file
+    dyear,
+    n,m,G,H,SecG,SecH,
+    ...
+
+    Args:
+        infilename(string ending in .COF): the .COF file that contains the
+            WMM coefficents, download this from https://www.ngdc.noaa.gov/geomag/WMM/DoDWMM.shtml
+        maxdegree(positive integer): maximum degree"""
+    with open(infilename,'r') as f:
+        s= f.read()
+        a= s.split('\n')
+        dyear= float(a[0].split()[0])
+        l=[dyear]
+        l.append([0,0,0.0,0.0,0.0,0.0])
+        for i in range(1,((maxdegree+1)*(maxdegree+2))//2):
+            rowa= a[i].split()
+            l.append([int(rowa[0]),int(rowa[1]),float(rowa[2]),float(rowa[3]),float(rowa[4]),float(rowa[5])])
+        return l
+
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    description="""
+    #Nathan Zimmerberg
+    #23 OCT 2019
+    #wmmcodeupdate.py
+    #This is a simple script to parse WMM.COF into a c++ header file
+    #it un Schmidt semi-normalizes the coefficents and stores them by:
+    #    index=((2*maxdegree-m+1)*m)/2+n
+    """)
+    parser.add_argument('-f',action='append',help="""the .COF files that contains the
+        WMM coefficents, download from https://www.ngdc.noaa.gov/geomag/WMM/DoDWMM.shtml.""")
+    parser.add_argument('-o',type=str,default='geomag.hpp',help='the c++ header filename to write the coefficents and model')
+    parser.add_argument('-n',type=int,default=12,help='maximum number of degrees to use')
+    arg=parser.parse_args()
+
+    main(arg.f,arg.o,arg.n)
